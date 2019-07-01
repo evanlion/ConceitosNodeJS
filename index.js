1d4666f3e1cfcd1f220d46327a7f17e17d12003b
Dev <evangelistalion@gmail.com>
2019-06-30 23:58:48 -0300

    start

---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+let numberOfRequests = 0;
+
+// Middleware
+function checkProjectExists(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id);
+
+  if (!project) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+
+  return next();
+}
+
+function logRequests(req, res, next) {
+  numberOfRequests++;
+
+  console.log(`Número de requisições: ${numberOfRequests}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+// PROJECTS
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const project = projects.find(p => p.id === id);
+
+  return res.json(project);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  var projectAlreadyExists = projects.find(p => p.id === id);
+
+  if (projectAlreadyExists) {
+    return res.status(400).json({ error: "Project already exists" });
+  }
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+// TASKS
+
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3001);
